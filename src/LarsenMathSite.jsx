d81d8f996cbcf9934d2f9b73909041161f7b9fe2
LARSENZA <larsenmbhoni@gmail.com>
2026-07-30 18:28:53 +0200

    Increase iframe height for booking lessons

--- src/LarsenMathSite.jsx
+++ src/LarsenMathSite.jsx
@@ -818,7 +818,7 @@ export default function LarsenMathSite() {
                   title="Book a lesson"
                   loading="lazy"
                   width="100%"
-                  height="500"
+                  height="1000"
                   frameBorder="0"
                   marginHeight="0"
                   marginWidth="0"
